874d3e950b7eb4847eb862848e6c9b87d3a947ba
Hans Larsen <hans@hansl.ca>
2016-04-25 09:35:42 -0700

    chore: add a skeleton for ts and project class

---
+++ src/project/project.ts
@@ -0,0 +1,18 @@
+import * as ts from 'typescript';
+
+/**
+ * An Angular application.
+ */
+export default class AngularApp {
+  private _angularCliPath: string;
+  private _angularCliJson: { [key: string]: any };
+
+  /**
+   * @param _tsConfigPath
+   */
+  constructor(private _tsConfigPath: string) {
+
+  }
+
+  
+}
